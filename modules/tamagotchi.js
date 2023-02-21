2ed6c353aa2537281a068774b4dc7fbc9d43ea5a
Danyeal <95497503+danyealmateen@users.noreply.github.com>
2023-02-21 16:10:45 +0100

    asdasd

--- modules/tamagotchi.js
+++ modules/tamagotchi.js
@@ -6,25 +6,33 @@ class Tamagotchi {
     this.#happiness = happiness;
   }
   addTamagotchi() {
+    //Hunger & Happiness stats
     this.#hunger = 10;
     this.#happiness = 10;
 
+    //Creatar & Hämtar element
+    let 
     let tamaContainer = document.getElementById("tamaContainer");
+    let tamaTitle = document.getElementById("tamaTitle");
     let feedButton = document.createElement("button");
     let happinessButton = document.createElement("button");
     let stats = document.createElement("div");
 
+    //Statsen innerText
     stats.innerHTML = `Hunger : ${this.#hunger}<br> Happiness : ${
       this.#happiness
     }`;
+    tamaTitle.innerText = `Testing title`;
 
     happinessButton.innerText = `Play`;
-    feedButton.innerText = `Feed`;
+    feedButton.innerText = `Feed`; 
 
+    //Append
     tamaContainer.appendChild(feedButton);
     tamaContainer.appendChild(happinessButton);
     tamaContainer.append(stats);
 
+    //Eventlistener
     feedButton.addEventListener("click", () => {
       console.log(123);
     });
